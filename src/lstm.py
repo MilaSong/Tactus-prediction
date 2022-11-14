807cd7bae64875343f9196f19e2abf57d79a7093
Milita Songailaite <songailaite.milita@gmail.com>
2022-11-14 22:57:36 +0200

    fix: fixed refactoring, added model plotting

--- src/lstm.py
+++ src/lstm.py
@@ -1,27 +1,31 @@
 import tensorflow as tf
+from keras.utils.vis_utils import plot_model
 import json
+CONFIG_PATH = "src/train_params.json"
 
 
-config = {
-    "hidden_lstm": 40,
-    "time_steps": 40
-}
+with open(CONFIG_PATH, "r") as f:
+    config = json.load(f)
 
 
 class LSTMModel(tf.keras.Model):
     def __init__(self):
-
+        super().__init__()
         self.model = tf.keras.Sequential( 
             [
-                tf.layers.LSTM(units = config.get("hidden_lstm", 10), activation = "tanh", input_shape=(config.get("time_steps", 10), 3)),
-                tf.layers.Dense(units = 1, activation = "linear"),
+                tf.keras.layers.LSTM(units = config.get("hidden_lstm", 10), activation = "tanh", input_shape=(config.get("time_steps", 10), 3)),
+                tf.keras.layers.Dense(units = 1, activation = "linear"),
                 #layers.Dropout(0.5),
-                tf.layers.RepeatVector(2),
-                tf.layers.LSTM(units = config.get("hidden_lstm", 10), activation = "tanh", return_sequences=True),
+                tf.keras.layers.RepeatVector(2),
+                tf.keras.layers.LSTM(units = config.get("hidden_lstm", 10), activation = "tanh", return_sequences=True),
                 #layers.Dropout(0.5),
-                tf.layers.TimeDistributed(tf.layers.Dense(units = 1, activation="sigmoid"))
+                tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units = 1, activation="sigmoid"))
             ]
         )
 
     def call(self, inputs):
         return self.model(inputs)
+
+model = LSTMModel()
+model.build(input_shape=(1, config.get("time_steps", 10), 3))
+plot_model(model.model, to_file='resources/lstm_structure.png', show_shapes=True, show_layer_names=True)
